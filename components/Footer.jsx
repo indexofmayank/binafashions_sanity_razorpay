7e703e9f0fece3d5fd531defdf0be6da53f767c1
mayanktiwaribaba <62905191+mayanktiwaribaba@users.noreply.github.com>
2022-05-11 15:47:03 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 JSM Headphones All rights reserverd</p>
+      <p>2022 ©BinaFashions powerd by <a href="https://www.Maaisha.in">Maaisha</a></p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
