d93caa36779860850beae813bbe46f30a24bbca5
Mayank Tiwari <62905191+indexofmayank@users.noreply.github.com>
2022-05-24 20:12:02 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Home = ({ products, bannerData }) => (
     <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
     <div className="products-heading">
       <h2>Best Seller Products</h2>
-      <p>Some more exculise producs from bina fashions</p>
+      <p>Some more exclusive producs from fashions</p>
     </div>
 
     <div className="products-container">
